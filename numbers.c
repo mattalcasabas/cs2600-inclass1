41c36ce8d1bc925b45bdb5616b73e7a6d1525623
Matthew Alcasabas <mialcasabas@cpp.edu>
2021-09-29 19:13:15 -0700

    Initial commit. Made numbers.c and restaurant.c with outlines

---
+++ numbers.c
@@ -0,0 +1,26 @@
+/*
+CS 2600
+In-class 1
+Part 2: Number guessing game
+Matthew Alcasabas
+
+1. Print out main menu
+	a. Press 1 to play a game
+	b. Press 2 to change the max number
+	c. Press 3 to quit
+
+2. If option 1 is selected:
+	a. Generate a random number between 1 and maxNumber (by default, 10)
+	b. User enters a number
+	c. If number too low, tell user, and have them guess again
+	d. If number too high, see c
+	e. If number is correct, go back to main menu
+
+3. If option 2 is selected:
+	a. Change maxNumber from 10 to whatever user inputs
+	b. See 2b
+
+4. If option 3 is selected:
+	a. Print "Thanks for playing!"
+	b. Exit
+*/
---
+++ restaurant.c
@@ -0,0 +1,13 @@
+/*
+CS 2600
+In-Class 1
+Part 1: Restaurant bill
+
+1. Generate random number. This number will be used to randomly choose an item from the menu
+2. Ask user for tax rate as a percentage
+3. Ask user for tip as a percentage (e.g. 15%)
+4. Print the meal cost
+5. Print the tax amount
+6. Print the tip amount
+7. Print the total bill
+*/
